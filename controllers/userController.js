1b34953924cf4232dff3a87a9e746836075d2855
Richard Jeremiah <myjkrichard@gmail.com>
2023-11-28 15:42:19 +0100

    fixed generate key import error for the crypto package

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import otpGenerator from "otp-generator";
 import { encrypt, randomizer } from "../connection/middleware/encyptor.js";
-import crypto, { generateKey } from 'crypto'
+import crypto from 'crypto'
 
 export const registerUser = async (req, res) => {
   try {
