faa12be1aef712cbf625627bc31748f2c8cb03f3
Adeyemi Abiodun <samuelabiodun377@gmail.com>
2023-11-29 20:08:45 +0530

    Chart

---
+++ src/components/Chart.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+import 'chartjs-plugin-datalabels';
+
+const LineChart = () => {
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        const ctx = chartRef.current.getContext('2d');
+
+        const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+        gradient.addColorStop(0, 'rgba(255, 99, 132, 0.6)');
+        gradient.addColorStop(1, 'rgba(255, 255, 255, 0.1)');
+
+        const data = {
+            labels: ['Total Invoices', 'Pending Invoices', 'Total Products', 'Completed Payments', 'Total Transactions'],
+            datasets: [
+                {
+                    label: 'Monthly Sales',
+                    data: [4, 56, 210, 70, 12, 80, 123],
+                    borderColor: 'rgba(34, 43, 70, 1)',
+                    borderWidth: 2,
+                    pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                    pointBorderColor: 'rgba(255, 255, 255, 0.8)',
+                    pointBorderWidth: 2,
+                    backgroundColor: gradient,
+                },
+            ],
+        };
+
+        const options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                datalabels: {
+                    anchor: 'end',
+                    align: 'end',
+                    backgroundColor: 'rgba(255, 99, 132, 0.8)',
+                    borderRadius: 4,
+                    color: 'white',
+                    font: {
+                        weight: 'bold',
+                    },
+                    formatter: (value) => `$${value}`,
+                },
+            },
+            scales: {
+                x: {
+                    type: 'category',
+                    grid: {
+                        display: false,
+                    },
+                },
+                y: {
+                    min: 0
+                },
+            },
+        };
+
+        const lineChart = new Chart(ctx, {
+            type: 'line',
+            data,
+            options,
+        });
+
+        // Cleanup the chart when the component unmounts
+        return () => {
+            lineChart.destroy();
+        };
+    }, []);
+
+    return (
+        <div style={{ background: "white", padding: "1rem", borderRadius: ".5rem", position: 'relative', height: '300px', width: '90%', marginTop: '3rem' }}>
+            <canvas ref={chartRef}></canvas>
+        </div>
+    );
+};
+
+export default LineChart;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { Header } from "../components"
+import LineChart from "../components/Chart"
 
 const Dashboard = () => {
   return (
@@ -13,24 +14,30 @@ const Dashboard = () => {
           <div className="w-40 p-2 shadow-2xl grid place-items-center text-blue-800 font-semibold font-poppins border rounded bg-white h-24">
             <span className="text-xs place-self-start">Total Invoices</span>
             <span className="text-3xl text-gray-700">210</span>
+            <img src="./src/assets/icons/total-invoice.png" alt="" className="w-4 place-self-end" />
           </div>
           <div className="w-40 p-2 shadow-2xl grid place-items-center text-blue-800 font-semibold font-poppins border rounded bg-white h-24">
             <span className="text-xs place-self-start">Pending Invoices</span>
             <span className="text-3xl text-gray-700">210</span>
+            <img src="./src/assets/icons/progress.png" alt="" className="w-5 place-self-end" />
           </div>
           <div className="w-40 p-2 shadow-2xl grid place-items-center text-blue-800 font-semibold font-poppins border rounded bg-white h-24">
             <span className="text-xs place-self-start">Total Products</span>
             <span className="text-3xl text-gray-700">210</span>
+            <img src="./src/assets/icons/product.png" alt="" className="w-5 place-self-end" />
           </div>
           <div className="w-40 p-2 shadow-2xl grid place-items-center text-blue-800 font-semibold font-poppins border rounded bg-white h-24">
             <span className="text-xs place-self-start">Completed Payments</span>
             <span className="text-3xl text-gray-700">210</span>
+            <img src="./src/assets/icons/payment.png" alt="" className="w-5 place-self-end" />
           </div>
           <div className="w-40 p-2 shadow-2xl grid place-items-center text-blue-800 font-semibold font-poppins border rounded bg-white h-24">
             <span className="text-xs place-self-start">Total Transactions</span>
             <span className="text-3xl text-gray-700">210</span>
+            <img src="./src/assets/icons/transaction.png" alt="" className="w-5 place-self-end" />
           </div>
         </div>
+        <LineChart />
       </div>
     </div>
   )
