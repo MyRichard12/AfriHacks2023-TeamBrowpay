e1e8f016fe5a2e746b6e51d48ada0622202ba642
Richard Jeremiah <myjkrichard@gmail.com>
2023-11-30 04:53:20 +0100

    completed the dynamic invoice prodict details section

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -3,15 +3,75 @@ import { Navbar } from '../components';
 import { motion } from 'framer-motion'
 import { staggerContainer, fadeIn, textVariant } from '../utils/motion'
 import { Container, Card, CssBaseline, FormControlLabel, TextField, Checkbox, Link, Paper, Avatar, Button, Box, Grid } from "@mui/material";
+import { useEffect, useState } from 'react';
 
 
 
 const defaultTheme = createTheme();
+
 function Sales() {
+  const productItems = [
+    {
+      product_name: '',
+      quantity: '',
+      unit_price: '',
+      price: ''
+    }
+  ]
+
+  const itemStub = {
+    product_name: '',
+    quantity: '',
+    unit_price: '',
+    price: ''
+  }
+
   const initialValues = {
     email: '',
     password: ''
+  }  
+
+  const [items, itemTrigger] = useState(productItems)
+  
+  const addItem = () => {
+    // my first error approach
+    // itemTrigger(items.push({
+    //   ...itemStub
+    // }))
+
+    // the correct approach
+    itemTrigger([...items, itemStub])
   }
+  
+  const removeItem = (index) => {
+    const last_index = items.length
+    
+    // console.log(index,last_index)
+
+    if(index  !== -1){
+      
+
+      // in this case the itemtrigger has to take on more than just the newarray the new array has to be rendered REACTIVELY within it
+      // error approach
+      // const updatedItem = item.slice()
+      //  updatedItem.splice(index, 1)
+      // itemTrigger(updatedItem)
+
+      // correct approach
+      itemTrigger(item => {
+        const updatedItem = item.slice()
+        updatedItem.splice(index, 1)
+
+        return updatedItem
+      })
+
+      console.log(items)
+    }
+    
+  }
+  
+
+
   return (
     <>
       <ThemeProvider theme={defaultTheme} />
@@ -38,6 +98,9 @@ function Sales() {
         <div className='max-w-[800px] mt-16'>
           <form action="" method="post" className=' bg-white p-4 border rounded-lg gap-6 shadow-lg'>
             <p className='mb-2 text-lg font-medium'>Customer Information</p>
+
+            {/* invoice fields */}
+  
             <TextField
               margin="normal"
               required
@@ -72,7 +135,62 @@ function Sales() {
                 className='input mb-4'
               />
             </div>
+
+            {/* product fields */}
             <p className='mb-2 text-lg font-medium'>Product Details</p>
+
+            {items.map((values, index) => {
+              return (
+              <InvoiceFields fields={values} key={index} index={index} remove={() => removeItem(index)}/>
+              );
+            })}
+
+            <div className="flex gap-10 mx-auto w-fit">
+            <div className="bg-green-500 w-fit py-2 px-4 text-white cursor-pointer" onClick={() => addItem()}>Add Product </div> 
+            </div>
+            
+
+            <Button
+              type="submit"
+              fullWidth
+              className="bg-orange-900"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              // onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </form>
+
+          
+
+
+        </div>
+        
+      </div>
+
+      
+      
+    </>
+  )
+}
+
+
+const InvoiceFields = ({fields, index, remove}) => {
+
+const removeItem = (index) => {
+  if(remove){
+    remove(index)
+  }
+}  
+
+
+  return(
+    <div>
+      <div className="flex justify-between">
+        <span>Item {index +1}</span>
+        <span className='text-gray-600 cursor-pointer' onClick={removeItem}>{index > 0 &&  'Remove'}</span>
+        </div>
             <div className="grid grid-cols-2 gap-3">
             <TextField
                 margin="normal"
@@ -117,29 +235,8 @@ function Sales() {
                 className='input mb-4'
               />
             </div>
-
-            <Button
-              type="submit"
-              fullWidth
-              className="bg-orange-900"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              // onClick={handleSubmit}
-            >
-              Submit
-            </Button>
-          </form>
-
-          
-
-
-        </div>
-        
-      </div>
-
-      
-      
-    </>
+            <div className="border-b-2" />
+            </div>
   )
 }
 
